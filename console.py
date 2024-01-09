e4f000ca2e4c768328ea66c16319283afb560d80
pasebukky <olubukunolapase@gmail.com>
2024-01-09 15:35:35 +0100

    Implement commands (quit, EOF, help and emptyline) and custom prompt (hbnh)

--- console.py
+++ console.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
+""" This program contains the entry point of the command interpreter """
 
 import cmd
 
-class HBNBCommand(cmd.Cmd):
 
+class HBNBCommand(cmd.Cmd):
+    """ This class handles commands on the console """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """ Quit command used to exit the program """
+        return True
+
+    def do_EOF(self, line):
+        """ EOF command used to exit the program """
+        return True
+
+    def emptyline(self):
+        """ Command to do nothing when an emptyline is encountered """
+        pass
 
 
 if __name__ == '__main__':
