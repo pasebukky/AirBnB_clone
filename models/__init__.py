1d36730e1924bfcbeb88d118d4b14e17b3f944cb
pasebukky <olubukunolapase@gmail.com>
2024-01-11 15:42:04 +0100

    Update file storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.base_model import BaseModel
 """
 Desc:
     when the application starts or when the models package is imported,
