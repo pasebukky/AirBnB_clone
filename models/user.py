c4763fb13647e75819a7cbfd6b95d7bf9bc15ba6
pasebukky <olubukunolapase@gmail.com>
2024-01-14 02:19:12 +0100

    Revert

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ All users personal information """
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
